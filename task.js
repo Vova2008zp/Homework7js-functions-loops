bb2d216ad4ca75e26c24a25529f853ab4cdd8c44
Vova2008zp <ironfoodua@gmail.com>
2022-09-07 15:40:29 +0300

    Add Stackoverflow 1,2,3

--- task.js
+++ task.js
@@ -10,13 +10,21 @@ const isAdult = function (num) {
 //Task 1.2
 const isAdult2 = function (num) {
   if (isNaN(num) && typeof num !== "number") {
-    console.log(null);
+    return null;
   } else if (num >= 18) {
     console.log(true);
   } else {
     console.log(false);
   }
 };
+
+//Task 1.3
+const isAdult3 = function (num) {
+  if (isNaN(num) && typeof num !== "number") {
+    return null;
+  }
+  num >= 18 ? console.log(true) : console.log(false);
+}
 
 //Task 2.1
 const checkMultiplicity = function (num1, num2) {
@@ -30,13 +38,21 @@ const checkMultiplicity = function (num1, num2) {
 //Task 2.2
 const checkMultiplicity2 = function (num1, num2) {
   if (isNaN(num1) || isNaN(num2)) {
-    console.log(null);
+    return null;
   } else if (num1 % num2 === 0) {
     console.log(true);
   } else {
     console.log(false);
   }
 };
+
+//Task 2.3
+const checkMultiplicity3 = function (num1, num2) {
+  if (isNaN(num1) || isNaN(num2)) {
+    return null;
+  }
+    num1 % num2 === 0 ? console.log(true) : console.log(false);
+  }
 
 //Task 3.1
 const lengthSideTriangle = function (a, b, c) {
@@ -50,10 +66,18 @@ const lengthSideTriangle = function (a, b, c) {
 //Task 3.2
 const lengthSideTriangle2 = function (a, b, c) {
   if (isNaN(a) || isNaN(b) || isNaN(c)) {
-    console.log(null);
+    return null;
   } else if (a + b > c && a + c > b && b + c > a) {
     console.log(true);
   } else {
     console.log(false);
   }
 };
+
+//Task 3.3
+const lengthSideTriangle3 = function (a, b, c) {
+  if (isNaN(a) || isNaN(b) || isNaN(c)) {
+    return null;
+  }
+  a + b > c && a + c > b && b + c > a ? console.log(true) : console.log(false);
+}
