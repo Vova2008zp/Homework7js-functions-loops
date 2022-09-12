5197f2e473dcd369d73ff9a1b60d09608dafaa91
Vova2008zp <ironfoodua@gmail.com>
2022-09-12 16:47:13 +0300

    After checking

--- task.js
+++ task.js
@@ -1,15 +1,14 @@
 // Task 1.1
-const isAdult = function (num) {
+const isAdult1 = function (num) {
   if (num >= 18) {
-    console.log(true);
-  } else {
-    console.log(false);
+    return true;
   }
+  return false;
 };
 
 //Task 1.2
 const isAdult2 = function (num) {
-  if (isNaN(num) && typeof num !== "number") {
+  if (isNaN(num) || typeof num !== "number") {
     return null;
   } else if (num >= 18) {
     console.log(true);
@@ -20,38 +19,36 @@ const isAdult2 = function (num) {
 
 //Task 1.3
 const isAdult3 = function (num) {
-  if (isNaN(num) && typeof num !== "number") {
+  if (isNaN(num) || typeof num !== "number") {
     return null;
   }
-  num >= 18 ? console.log(true) : console.log(false);
+  return num >= 18;
 };
 
 //Task 2.1
-const checkMultiplicity = function (num1, num2) {
+const checkMultiplicity1 = function (num1, num2) {
   if (num1 % num2 === 0) {
-    console.log(true);
-  } else {
-    console.log(false);
+    return true;
   }
+  return false;
 };
 
 //Task 2.2
 const checkMultiplicity2 = function (num1, num2) {
-  if (isNaN(num1) || isNaN(num2)) {
+  if (isNaN(num1 - num2)) {
     return null;
   } else if (num1 % num2 === 0) {
-    console.log(true);
-  } else {
-    console.log(false);
+    return true;
   }
+  return false;
 };
 
 //Task 2.3
 const checkMultiplicity3 = function (num1, num2) {
-  if (isNaN(num1) || isNaN(num2)) {
+  if (isNaN(num1 - num2)) {
     return null;
   }
-  num1 % num2 === 0 ? console.log(true) : console.log(false);
+  return num1 % num2 === 0;
 };
 
 //Task 3.1
@@ -65,7 +62,7 @@ const lengthSideTriangle = function (a, b, c) {
 
 //Task 3.2
 const lengthSideTriangle2 = function (a, b, c) {
-  if (isNaN(a) || isNaN(b) || isNaN(c)) {
+  if (isNaN(a - b - c)) {
     return null;
   } else if (a + b > c && a + c > b && b + c > a) {
     return true;
@@ -76,30 +73,32 @@ const lengthSideTriangle2 = function (a, b, c) {
 
 //Task 3.3
 const lengthSideTriangle3 = function (a, b, c) {
-  if (isNaN(a) || isNaN(b) || isNaN(c)) {
+  if (isNaN(a - b - c)) {
     return null;
   }
-  a + b > c && a + c > b && b + c > a ? console.log(true) : console.log(false);
+  return a + b < c && a + c < b && b + c < a;
 };
 
 //Task 4
-
 const getSumOddNumber = function (num) {
-  let result = 0;
-  debugger;
-  for (let i = 1; i < num; i++) {
-    result = num + i;
-    if(result % 2 === 0) continue;
-    else { result +=result};
+  if (typeof num !== "number" || isNaN(num)) {
+    return null;
   }
-  // return result;
+  let result = 0;
+  for (let i = 0; i <= num; i++) {
+    if (i % 2 === 0) continue;
+    result += i;
+  }
+  return result;
 };
 
 //Task 5
 function isPrimeNumber(num) {
-  // debugger
+  if (typeof num !== "number" || isNaN(num)) {
+    return null;
+  }
   for (let i = 2; i < num; i++) {
-    if ( num % i === 0) return false;
+    if (num % i === 0) return false;
   }
   return true;
 }
